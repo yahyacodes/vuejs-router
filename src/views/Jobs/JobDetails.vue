8a7de76828d3136dd17000080ed93ea83503c3d7
yahyacodes <yahya.codin@gmail.com>
2023-01-19 11:21:15 +0300

    m

--- src/views/Jobs/JobDetails.vue
+++ src/views/Jobs/JobDetails.vue
@@ -1,18 +1,28 @@
 <template>
-  <div>
-    <h1>Job Details Page</h1>
-    <p>The job id is {{ id }}</p>
+  <div v-if="job">
+    <h1>{{ job.title }}</h1>
+    <p>{{ id }}</p>
+    <p>{{ job.details }}</p>
+  </div>
+  <div v-else>
+    <p>Loading....</p>
   </div>
 </template>
 
 <script>
 export default {
   props: ["id"],
-  //   data() {
-  //     return {
-  //       id: this.$route.params.id,
-  //     };
-  //   },
+  data() {
+    return {
+      job: null,
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/jobs/" + this.id)
+      .then((res) => res.json())
+      .then((data) => (this.job = data))
+      .catch((err) => console.log(err.message));
+  },
 };
 </script>
 
